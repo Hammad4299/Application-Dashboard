cbab3d82d88dccac46e983c31e8dfd628d860ee1
Hammad <hammad4299@gmail>
2018-10-03 04:14:37 +0500

    no message

--- docker/scripts/substitute-static-content-url.sh
+++ docker/scripts/substitute-static-content-url.sh
@@ -3,7 +3,7 @@ cd $VOLUME_PATH
 
 #envsubst doesn't perform inplace substitution
 mv webpack.env.ts tmp && envsubst '${STATIC_CONTENT_URL}' < tmp > webpack.env.ts && rm tmp > /dev/null
-mv public/webpack-manifest.json tmp && envsubst '${STATIC_CONTENT_URL}' < tmp > public/webpack-manifest.json && rm tmp  > /dev/null
+mv webpack-assets.json tmp && envsubst '${STATIC_CONTENT_URL}' < tmp > webpack-assets.json && rm tmp  > /dev/null
 
 REGEX=".*\.\(js\|js\.map\|css\|css\.map\)$"
 find public -regex $REGEX | while read line; do
