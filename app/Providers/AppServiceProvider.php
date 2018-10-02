14a6950ccf1efc8dcb5b0533d166c804dde98273
Hammad <hammad4299@gmail>
2018-10-03 03:45:39 +0500

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use App\Validator\ValidatorWithCode;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\URL;
+use App\Validator\ValidatorWithCode;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
 //            }
 //        );
         if(config('app.forceScheme')!=null) {
-            Illuminate\Support\Facades\URL::forceScheme(config('app.forceScheme'));
+            URL::forceScheme(config('app.forceScheme'));
         }
 
         Validator::resolver(function($translator, $data, $rules, $messages)
