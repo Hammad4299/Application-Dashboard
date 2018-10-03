500961f75c2df3580a92c18d967290339a01badf
Hammad <hammad4299@gmail>
2018-10-04 00:32:41 +0500

    no message

--- docker/scripts/init-cron.sh
+++ docker/scripts/init-cron.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd $SCRIPTS_DIR
 sh substitute-static-content-url.sh
-sh permission-set.sh
 sh laravel-common.sh
 sh permission-set.sh
 sh start-cron.sh
--- docker/scripts/init-dev-php.sh
+++ docker/scripts/init-dev-php.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd $SCRIPTS_DIR
 sh composer-install.sh
-sh permission-set.sh
 sh laravel-common-dev.sh
 sh permission-set.sh
 mv /scripts/xdebug.ini tmp && envsubst '${XDEBUG_REMORT_PORT}' < tmp > /usr/local/etc/php/conf.d/xdebug.ini && rm tmp > /dev/null
