63105afd470557fba592cd1ccf42173c52e25e3c
Hammad <hammad4299@gmail>
2019-06-11 14:20:52 +0500

    no msg

--- devops/helm/ci/prepare.sh
+++ devops/helm/ci/prepare.sh
@@ -10,6 +10,6 @@ apk add --update libintl && \
     apk add --virtual build_deps gettext git &&  \
     cp /usr/bin/envsubst /usr/local/bin/envsubst
 
-mv devops/helm/charts/appname/values.yaml devops/helm/charts/appname/tmpvalues.yaml \
-    && envsubst '${image_repo} ${image_tag} ${image_executor_repo} ${image_executor_tag}' < devops/helm/charts/appname/tmpvalues.yaml > devops/helm/charts/appname/values.yaml \
-    && rm devops/helm/charts/appname/tmpvalues.yaml
+mv devops/helm/charts/application-dashboard-web/values.yaml devops/helm/charts/application-dashboard-web/tmpvalues.yaml \
+    && envsubst '${image_repo} ${image_tag} ${image_executor_repo} ${image_executor_tag}' < devops/helm/charts/application-dashboard-web/tmpvalues.yaml > devops/helm/charts/application-dashboard-web/values.yaml \
+    && rm devops/helm/charts/application-dashboard-web/tmpvalues.yaml
